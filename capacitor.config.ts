cb89d07caa4aa79207fe4e93ca7407a0f3e6676f
Joris-G <joris-grangier@orange.fr>
2024-07-07 15:36:49 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jwd.aet.light',
+  appName: 'jwd-aet-light',
+  webDir: 'www'
+};
+
+export default config;
